fcd2fcd6ec394f240f08ce5360f98c85416f3952
EnzoCarrillo <enzocarrillo25@gmail.com>
2023-10-13 20:57:43 -0300

    añadiendo funciones basicas

--- pantallas/registro.js
+++ pantallas/registro.js
@@ -1,2 +1,9 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+export default function RegisterScreen({ navigation }) {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const handleRegister = () => {
+      alert('Registro exitoso. ¡Bienvenido, ' + username + '!');
+    };
