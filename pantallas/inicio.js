a2fcd75bc86e456c271106f2a9a09dd9b8bd7c8a
EnzoCarrillo <enzocarrillo25@gmail.com>
2023-10-13 22:31:04 -0300

    arreglando el codigo que hice mal

--- pantallas/inicio.js
+++ pantallas/inicio.js
@@ -1,2 +1,17 @@
-import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import { TextToSpeechClient } from '@google-cloud/text-to-speech';
+
+const client = new TextToSpeechClient();
+
+export default function HomeScreen() {
+  const [textToSpeak, setTextToSpeak] = useState('');
+  const [audioUrl, setAudioUrl] = useState('');
+
+  const handleTextToSpeech = async () => {
+    try {
+      const [response] = await client.synthesizeSpeech({
+        input: { text: textToSpeak },
+        voice: { languageCode: 'en-US', ssmlGender: 'NEUTRAL' },
+        audioConfig: { audioEncoding: 'MP3' },
+      });
