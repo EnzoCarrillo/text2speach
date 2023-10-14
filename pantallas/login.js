289ec92f8ffbd271422392f28849dfcfcee96e62
EnzoCarrillo <enzocarrillo25@gmail.com>
2023-10-13 22:06:35 -0300

    añadiendo funciones basicas

--- pantallas/login.js
+++ pantallas/login.js
@@ -1,2 +1,10 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+export default function LoginScreen({ navigation }) {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  
+    const handleLogin = () => {
+      alert('Inicio de sesión exitoso. ¡Bienvenido, ' + username + '!');
+    };
+    
