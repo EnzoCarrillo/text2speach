adf8fe8ee92e2d5966d73b8a789d8e93c4ae7d8e
EnzoCarrillo <enzocarrillo25@gmail.com>
2023-10-13 21:14:24 -0300

    terminando con el codigo del registro

--- pantallas/registro.js
+++ pantallas/registro.js
@@ -7,3 +7,29 @@ export default function RegisterScreen({ navigation }) {
     const handleRegister = () => {
       alert('Registro exitoso. ¡Bienvenido, ' + username + '!');
     };
+    return (
+        <View style={styles.container}>
+          <Text style={styles.title}>Registro</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Nombre de usuario"
+            value={username}
+            onChangeText={text => setUsername(text)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Correo electrónico"
+            value={email}
+            onChangeText={text => setEmail(text)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Contraseña"
+            secureTextEntry={true}
+            value={password}
+            onChangeText={text => setPassword(text)}
+          />
+          <Button title="Registrarse" onPress={handleRegister} />
+        </View>
+      );
+    }
