79684e7124152a8e7936fb62f5c219ebf5b0ca6d
EnzoCarrillo <enzocarrillo25@gmail.com>
2023-10-13 22:13:13 -0300

    añadiendo intento de css

--- pantallas/login.js
+++ pantallas/login.js
@@ -27,3 +27,22 @@ export default function LoginScreen({ navigation }) {
         </View>
       );
     }
+    const styles = StyleSheet.create({
+        container: {
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        title: {
+          fontSize: 24,
+          marginBottom: 20,
+        },
+        input: {
+          width: '80%',
+          height: 40,
+          borderColor: 'gray',
+          borderWidth: 1,
+          marginBottom: 10,
+          paddingHorizontal: 10,
+        },
+      });
