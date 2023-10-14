bb320677243456025e29ab5ed6439a8df273685d
EnzoCarrillo <enzocarrillo25@gmail.com>
2023-10-13 22:38:49 -0300

    añadiendole css al inicio

--- pantallas/inicio.js
+++ pantallas/inicio.js
@@ -42,3 +42,22 @@ return (
   </View>
 );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+});
