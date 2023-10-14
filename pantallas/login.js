d5b3c81ce4ae0a2c9a81c1b9a98f6a8ac548693c
EnzoCarrillo <enzocarrillo25@gmail.com>
2023-10-13 22:11:08 -0300

    terminando con la pantalla de login

--- pantallas/login.js
+++ pantallas/login.js
@@ -7,4 +7,23 @@ export default function LoginScreen({ navigation }) {
     const handleLogin = () => {
       alert('Inicio de sesión exitoso. ¡Bienvenido, ' + username + '!');
     };
-    
+    return (
+        <View style={styles.container}>
+          <Text style={styles.title}>Iniciar sesión</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Nombre de usuario"
+            value={username}
+            onChangeText={text => setUsername(text)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Contraseña"
+            secureTextEntry={true}
+            value={password}
+            onChangeText={text => setPassword(text)}
+          />
+          <Button title="Iniciar sesión" onPress={handleLogin} />
+        </View>
+      );
+    }
