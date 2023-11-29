723954021389412cdeffa2aa0c181037c4ecee41
EnzoCarrillo <enzocarrillo25@gmail.com>
2023-11-29 12:39:26 -0300

    empezando con firebase

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import LogScreen from './LogScreen';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './pantallas/login';
@@ -9,6 +17,35 @@ export default function App() {
   );
 }
 
+const Stack = createStackNavigator();
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAxNiwMZhcyu96Hjt9vDs2p86O1-JHE9HI',
+  authDomain: 'text2speach-f506f.firebaseapp.com',
+  projectId: 'text2speach-f506f',
+  storageBucket: 'text2speach-f506f.appspot.com',
+  messagingSenderId: '964372714905',
+  appId: '1:964372714905:web:51f98d9e86d3c2b279af08',
+  measurementId: "G-7FG8FN95Z4"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Logs" component={LogScreen} />
+        {/* Add other screens here */}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 
 const styles = StyleSheet.create({
