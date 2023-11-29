7612d845dd51dc1b027e3f57c61c4a1eaec344ad
EnzoCarrillo <enzocarrillo25@gmail.com>
2023-11-29 12:11:28 -0300

    correcciones de errores

--- App.js
+++ App.js
@@ -1,36 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import LoginScreen from './pantallas/login';
+import RegisterScreen from './pantallas/registro';
+import HomeScreen from './pantallas/inicio';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <HomeScreen />
   );
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  const registrationForm = document.getElementById('registrationForm');
-  registrationForm.addEventListener('submit', function (event) {
-  event.preventDefault();
-  const username = document.getElementById('username').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  alert('Registro exitoso. ¡Bienvenido, ' + username + '!');
-  });
-  });
 
-document.addEventListener('DOMContentLoaded', function () {
-    const loginForm = document.getElementById('loginForm');
-    loginForm.addEventListener('submit', function (event) {
-      event.preventDefault();
-      
-      const username = document.getElementById('username').value;
-      const password = document.getElementById('password').value;
-      alert('Inicio de sesión exitoso. ¡Bienvenido, ' + username + '!');
-      });
-      });
 
 const styles = StyleSheet.create({
   container: {
