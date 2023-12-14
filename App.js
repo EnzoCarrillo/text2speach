28d6a0e520a5be23bee22326aafbabe41e823ba9
EnzoCarrillo <enzocarrillo25@gmail.com>
2023-12-14 20:02:03 -0300

    configurando firebase devuelta

--- App.js
+++ App.js
@@ -2,27 +2,12 @@ import React, { useEffect } from 'react';
 import Navigation from './navigation';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import Inicio from './pantallas/inicio';
-import { initializeApp } from "firebase/app";
-import 'firebase/auth';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+import { firebaseConfig } from './firebase-config';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-export { auth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAxNiwMZhcyu96Hjt9vDs2p86O1-JHE9HI",
-  authDomain: "text2speach-f506f.firebaseapp.com",
-  projectId: "text2speach-f506f",
-  storageBucket: "text2speach-f506f.appspot.com",
-  messagingSenderId: "964372714905",
-  appId: "1:964372714905:web:51f98d9e86d3c2b279af08",
-  measurementId: "G-7FG8FN95Z4"
-};
 
 export default function App() {
-  useEffect(() => {
-    const app = initializeApp(firebaseConfig);
-  }, []);
-
   return (
       <Navigation />
   );
---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAxNiwMZhcyu96Hjt9vDs2p86O1-JHE9HI",
+    authDomain: "text2speach-f506f.firebaseapp.com",
+    projectId: "text2speach-f506f",
+    storageBucket: "text2speach-f506f.appspot.com",
+    messagingSenderId: "964372714905",
+    appId: "1:964372714905:web:51f98d9e86d3c2b279af08",
+    measurementId: "G-7FG8FN95Z4"
+};
