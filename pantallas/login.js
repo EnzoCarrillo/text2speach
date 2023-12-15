46a1e78bf30ca4aa0a9700029b1cb032bd2126b0
EnzoCarrillo <enzocarrillo25@gmail.com>
2023-12-15 13:50:54 -0300

    rediseñe la pantalla de login para que cumpla tanto la funcion de login como la de registro

--- pantallas/login.js
+++ pantallas/login.js
@@ -1,39 +1,41 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
-import { signInWithEmailAndPassword } from 'firebase/auth';
+import { Image, Text, StyleSheet, View, ScrollView, TouchableOpacity, TextInput, Button } from 'react-native';
+import { BlurView } from 'expo-blur';
 
-export default function LoginScreen({ navigation }) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const uri = 'https://e0.pxfuel.com/wallpapers/738/89/desktop-wallpaper-simple-minimalistic-best-phone-background-no-distractions-scenery-painting-nature-simple-sunset.jpg';
+const profilePicture = 'https://avatars.mds.yandex.net/i?id=15fbc484b9c87922e4855105070a0f5a25fee8d7-9094507-images-thumbs&n=13';
 
-  const handleLogin = async () => {
-    try {
-      await signInWithEmailAndPassword(auth, username, password);
-
-      Alert.alert('Inicio de sesión exitoso', `¡Bienvenido, ${username}!`, [{ text: 'OK', onPress: () => navigation.goBack() }]);
-    } catch (error) {
-      console.error('Error al iniciar sesión:', error.message);
-      Alert.alert('Error', 'Inicio de sesión fallido. Por favor, verifica tus credenciales.');
-    }
-  };
-
+export default function LoginScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Iniciar sesión</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Nombre de usuario o correo electrónico"
-        value={username}
-        onChangeText={(text) => setUsername(text)}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Contraseña"
-        secureTextEntry={true}
-        value={password}
-        onChangeText={(text) => setPassword(text)}
-      />
-      <Button title="Iniciar sesión" onPress={handleLogin} />
+      <Image source={{ uri }} style={[styles.image, StyleSheet.absoluteFill]} />
+      <ScrollView contentContainerStyle={{
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <BlurView intensity={90}>
+          <View style={styles.login}>
+            <Image source={{ uri: profilePicture }} style={styles.profilePicture} />
+            <View>
+              <Text style={{ fontSize: 17, fontWeight: '400', color: 'white' }}>E-mail</Text>
+              <TextInput style={styles.input} placeholder='exampleE-mail@gmail.com' />
+            </View>
+            <View>
+              <Text style={{ fontSize: 17, fontWeight: '400', color: 'white' }}>Password</Text>
+              <TextInput style={styles.input} placeholder='password' secureTextEntry={true} />
+            </View>
+            <TouchableOpacity style={[styles.button, { backgroundColor: '#00CFEB90' }]}>
+              <Text style={{ fontSize: 17, fontWeight: '400', color: 'white' }}>Login</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={[styles.button, { backgroundColor: '#6792F090' }]}>
+              <Text style={{ fontSize: 17, fontWeight: '400', color: 'white' }}>Create Account</Text>
+            </TouchableOpacity>
+          </View>
+        </BlurView>
+      </ScrollView>
     </View>
   );
 }
@@ -41,19 +43,52 @@ export default function LoginScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+  },
+  login: {
+    width: 350,
+    height: 500,
+    borderColor: '#fff',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
     alignItems: 'center',
   },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
+  profilePicture: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderColor: '#fff',
+    borderWidth: 1,
+    marginVertical: 30,
   },
   input: {
-    width: '80%',
+    width: 250,
     height: 40,
-    borderColor: 'gray',
+    borderColor: '#fff',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#ffffff90',
+    marginBottom: 20,
+  },
+  button: {
+    width: 250,
+    height: 40,
+    borderRadius: 10,
+    backgroundColor: '#00CFEB90',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
     borderWidth: 1,
-    marginBottom: 10,
-    paddingHorizontal: 10,
+    borderColor: '#fff',
   },
 });
